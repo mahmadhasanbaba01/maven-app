2d306b1bc440df98174813e85174277920aa7040
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2024-07-25 13:58:26 +0530

    added maven-web application

---
+++ aws-devops-repo/com.babait/src/test/java/com/baba/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
